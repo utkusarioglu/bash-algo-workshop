4528e2283a372f7ccd53577a304f6948a9941add
Utku Sarioglu <utkusarioglu@hotmail.com>
2024-04-10 08:17:32 +0000

    Feat: Bubble sort

---
+++ src/sorting-algorithms/bubble-sort.sh
@@ -0,0 +1,25 @@
+function bubble_sort {
+  local -a unsorted=($@)
+  local -i has_changed=1
+  
+  while [ $has_changed -eq 1 ]; do
+    has_changed=0
+    for (( l=1; l < ${#unsorted[@]}; l++ )); do
+      s=$(( l - 1 ))
+      v_large=${unsorted[$l]}
+      v_small=${unsorted[$s]}
+      
+      if [ $v_small -gt $v_large ]; then
+        unsorted[$l]=$v_small
+        unsorted[$s]=$v_large
+        has_changed=1
+      fi
+    done
+  done
+
+  echo "${unsorted[*]}"
+}
+
+function main {
+  bubble_sort "$@"  
+}
